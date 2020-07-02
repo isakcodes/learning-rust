01f32e8441e08ec8a2322b2941a05608a195863a
isakcodes <61279302+isakcodes@users.noreply.github.com>
2020-07-02 20:29:43 +0200

    Add try of concise error reporting

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -10,21 +10,24 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::from_args();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     //println!("Provided arguments: {} {}", args.pattern, args.path.display());
-    let file = File::open(&args.path).expect("Could not read file");
-    let reader = BufReader::new(file);
-    // let content = std::fs::read_to_string(&args.path)
-    //     .expect("could not read file");
-    for result in reader.lines() {
-        match result {
-            Ok(line) => {
-                if line.find(&args.pattern).is_some() {
-                    println!("{}", line);
-                }
-            }
-            Err(error) => { println!("Oh no, {}", error) }
-        }
-    }
+    
+    let args = Cli::from_args();
+    
+    // let file = File::open(&args.path).expect("Could not read file");
+    // let reader = BufReader::new(file);
+    // for result in reader.lines() {
+    //     match result {
+    //         Ok(line) => {
+    //             if line.find(&args.pattern).is_some() {
+    //                 println!("{}", line);
+    //             }
+    //         }
+    //         Err(error) => { println!("Oh no, {}", error) }
+    //     }
+    // }
+    let content = std::fs::read_to_string(&args.path)?;
+    println!("file content: {}", content);
+    Ok(())
 }
