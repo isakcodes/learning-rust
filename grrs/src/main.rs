f73ff320b3f03de02ebbf5dff2951650a4f96845
isakcodes <61279302+isakcodes@users.noreply.github.com>
2020-07-02 20:03:37 +0200

    Add optimization, using buffered reader instead of reading the entire file as a string

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -10,10 +13,12 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     //println!("Provided arguments: {} {}", args.pattern, args.path.display());
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
+    let file = File::open(&args.path).expect("Could not read file");
+    let reader = BufReader::new(file);
+    // let content = std::fs::read_to_string(&args.path)
+    //     .expect("could not read file");
+    for line in reader.lines().map(|l| l.unwrap()) {
+        if line.find(&args.pattern).is_some() {
             println!("{}", line);
         }
     }
